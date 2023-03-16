783a5dc88361fd686f598301dfcb9dfb6347caca
Abhinav Aggarwal <aggarwalabhinav2002@gmail.com>
2023-03-16 16:47:19 +0530

    Updates

---
+++ precache-manifest.90d8083bee972d037906330fd0984176.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49454e0262f04054f8c24b1ead20144d",
+    "url": "/react_noteTaking/index.html"
+  },
+  {
+    "revision": "655913e6e1c097986d9b",
+    "url": "/react_noteTaking/static/js/2.b445fd26.chunk.js"
+  },
+  {
+    "revision": "7f53e25218061792ae00",
+    "url": "/react_noteTaking/static/js/main.6f63ef98.chunk.js"
+  },
+  {
+    "revision": "24b6d251efdd0056e233",
+    "url": "/react_noteTaking/static/js/runtime-main.fb18418f.js"
+  }
+]);
